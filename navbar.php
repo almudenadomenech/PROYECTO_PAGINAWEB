13c16ff366f6b91f13309521b5cf8b8ed8b8ae74
almudenadomenech <violeta_33@hotmail.es>
2025-02-11 13:23:52 +0100

    añadir el navbar y el footer

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<section class="header">
+
+    <a href="home.php" class= "logo"> viajar.</a>
+
+    <nav class="navbar">
+        <a href="home.php">Home</a>
+        <a href="acercaDe.php">Acerca de</a>
+        <a href="paquetes.php">Paquetes</a>
+        <a href="booking.php">Booking</a>
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </nav>
+
+   
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
--- paquetes.php
+++ paquetes.php
@@ -17,19 +17,9 @@
 </head>
 <body>
 
-<section class="header">
-
-    <a href="home.php" class= "logo"> viajar.</a>
-
-    <nav class="navbar">
-        <a href="home.php">Home</a>
-        <a href="acercaDe.php">Acerca de</a>
-        <a href="paquetes.php">Paquetes</a>
-        <a href="booking.php">Booking</a>
-    </nav>
-    <div id="menu-btn" class="fas fa-bars"></div>
-
-</section>
+<?php
+    include('navbar.php');
+?>
 
 <div class="heading" style="background:url(images/header-2.jpg) no-repeat" >
     <h1>Paquetes</h1>
@@ -185,46 +175,11 @@
 
 
   <!-- seccion footer   -->
-<section class="footer">
+  <?php
+    include('footer.php');
+?>
 
-    <div class="box-container">
 
-        <div class="box">
-            <h3>Enlaces rápidos</h3>
-        <a href="home.php"> <i class="i.fas.fa-angle-right"></i>  Home</a>
-        <a href="acercaDe.php"> <i class="i.fas.fa-angle-right"></i>Sobre nosotros</a>
-        <a href="paquetes.php"><i class="i.fas.fa-angle-right"></i>Paquetes</a>
-        <a href="booking.php"><i class="i.fas.fa-angle-right"></i>Booking</a>
-        </div>
-
-        <div class="box">
-            <h3> Enlaces adicionales</h3>
-        <a href="#"> <i class="i.fas.fa-angle-right"></i>Preguntas</a>
-        <a href="#"> <i class="i.fas.fa-angle-right"></i>Sobre nosotros</a>
-        <a href="#"> <i class="i.fas.fa-angle-right"></i>Política de privacidad</a>
-        <a href="#"> <i class="i.fas.fa-angle-right"></i>Condiciones de uso</a>
-        </div>
-
-        <div class="box">
-            <h3>Información de contacto</h3>
-        <a href="#"> <i class="i.fas.fa-phone"> </i>785-75-69-98</a>
-        <a href="#"> <i class="i.fas.fa-phone"> </i>111-22-22-33</a>
-        <a href="#"> <i class="i.fas.fa-envelope"> </i>viajes@gmail.com</a>
-        <a href="#"> <i class="i.fas.fa-map"> </i>madrid, españa -28045</a>
-        </div>
-
-        <div class="box">
-            <h3>Síguenos</h3>
-            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-        </div>
-
-    </div>
-
-    
-</section>
 
 <!-- swiper js link  -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
