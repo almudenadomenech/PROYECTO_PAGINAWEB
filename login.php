23dcc31af0dccbf7ad8c02f9f4e16c65f9ecc0b8
almudenadomenech <violeta_33@hotmail.es>
2025-02-12 13:54:54 +0100

    modificaciones

--- login.php
+++ login.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+<?php
+    include('navbar.php');
+?>
 
     <div class="container-all">
 
@@ -39,7 +43,9 @@
             <p class="text-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque dignissimos mollitia quos nemo cum, error numquam amet suscipit architecto.</p>
         </div>
     </div>
-
+    <?php
+    include('footer.php');
+?>
 </body>
 
 </html>
